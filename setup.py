05832677daa57b643efd187cc1dc5a3dab367bfe
FireHead90544 <rudranshjoshi1806@gmail.com>
2024-07-28 03:21:24 +0530

    build: create build file

---
+++ setup.py
@@ -0,0 +1,31 @@
+from core import __version__, __author__
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as f:
+    readme = f.read()
+
+with open("requirements.txt", "r") as f:
+    requirements = f.read().strip().split("\n")
+
+setup(
+    name="how-cli",
+    version=__version__,
+    description="An AI-based CLI assistant to help you with command line & shell.",
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    url="https://github.com/FireHead90544/how-cli",
+    project_urls={
+        "Issue Tracker": "https://github.com/FireHead90544/how-cli/issues",
+    },
+    author=__author__,
+    author_email="rudranshjoshi1806@gmail.com",
+    platforms="any",
+    license="MIT",
+    packages=find_packages(),
+    install_requires=requirements,
+    entry_points={
+        "console_scripts": [
+            "how=how:app",
+        ]
+    }
+)
