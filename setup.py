56720b39715ad9c832a021d82048d648683e9831
FireHead90544 <rudranshjoshi1806@gmail.com>
2024-07-28 20:54:55 +0530

    fix: build unicode error

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from how import __version__, __author__
 from setuptools import setup, find_packages
 
-with open("README.md", "r") as f:
+with open("README.md", "r", encoding="utf-8") as f:
     readme = f.read()
 
 with open("requirements.txt", "r") as f:
